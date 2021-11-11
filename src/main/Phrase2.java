05f9fe07b5d9d6d60da201ed572fb19c411ae0b2
Carol benavides <carolbenavides@MacBook-Air-de-Carol.local>
2021-11-11 10:37:04 +0100

    first commit

---
+++ src/main/Phrase2.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Phrase2 {
+
+    public static void main(String[]args) {
+
+        System.out.println("Uno que madrugó, un dólar se encontró. Pero más madrugó, aquel que lo perdió.");
+    }
+
+}
